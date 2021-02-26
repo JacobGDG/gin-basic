6bdb56cc39e423c2b664fb3178e0a74f36286cee
JacobGDG <jacob.greenwood@gmail.com>
2021-02-26 14:34:23 +0000

    Basic index page and Make changes
    A few new files here. We are creating a bunch of basic HTML templates
    using the Bootstrap framework. This has given us a basic index page with
    a nav bar and empty footer.
    
    Slight changes to Makefile to allow for quicker testing locally. We
    don't need to build a new container every time.

--- Makefile
+++ Makefile
@@ -1,12 +1,13 @@
 APP_NAME ?= gin-basic
-APP_TAG ?= master
 APP_PORT ?= 8080
 
 build:
 	docker build -t ${APP_NAME} .
 
-start:
+start-build:
 	docker run -p ${APP_PORT}:${APP_PORT} \
 		${APP_NAME}
 
+start:
+	go mod tidy && go run .
 
--- main.go
+++ main.go
@@ -1,17 +1,30 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+  "net/http"
+
+  "github.com/gin-gonic/gin"
+)
 
 func main() {
+  // create a router object using the "default" one provided by Gin
   r := gin.Default()
+
+  // process html pages at the start so they don't have to be loaded again.
+  r.LoadHTMLGlob("templates/*")
+
+  r.GET("/", func(c *gin.Context) {
+    c.HTML(
+      http.StatusOK,
+      "index.html",
+      gin.H{
+        "title": "Basic Gin!",
+      },
+    )
+  })
   r.GET("/ping", func(c *gin.Context) {
     c.JSON(200, gin.H{
       "message": "pong",
-    })
-  })
-  r.GET("/ping-two", func(c *gin.Context) {
-    c.JSON(200, gin.H{
-      "message": "pong 2 test again",
     })
   })
   r.Run()
